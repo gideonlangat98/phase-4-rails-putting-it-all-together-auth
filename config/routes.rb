eaf4f0c6e6fdb929eb1c2454b8f5240a640e003d
gideonlangat98 <gideonlangat98@gmail.com>
2022-10-11 00:23:53 +0300

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/signup', to: 'users#create'
+  post '/login', to: 'sessions#create'
+  post 'recipes', to: 'recipes#create'
+  get '/me', to: 'users#show'
+  get '/recipes', to: 'recipes#index'
+  delete '/logout', to: 'sessions#destroy'
 end
